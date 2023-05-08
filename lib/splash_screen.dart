55f83f17cc3cc4c1e14583bd60918e5d421f7f3f
asmaa elbanna <asmaaelbanna21@gmail.com>
2023-05-08 21:09:01 +0300

    add animation

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -7,8 +7,23 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
 
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation = Tween<Offset>(begin: const Offset(0,8),end: Offset.zero).animate(animationController);
+    animationController.forward();
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +34,14 @@ class _SplashScreenState extends State<SplashScreen> {
         children: [
           Image.asset('assets/images/Logo.png'),
           const SizedBox(height: 5),
-          Text('Read free books', textAlign: TextAlign.center),
+          AnimatedBuilder(
+            animation: slidingAnimation,
+            builder: (BuildContext context, Widget? child) {
+              return SlideTransition(
+                  position: slidingAnimation  , child: Text('Read free books', textAlign: TextAlign.center));
+
+            },
+          ),
         ],
       ),
     );
