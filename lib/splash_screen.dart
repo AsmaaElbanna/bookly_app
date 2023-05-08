784cdb67205247ad72b72ab06e24145c9e9193ab
asmaa elbanna <asmaaelbanna21@gmail.com>
2023-05-08 13:36:26 +0300

    Initial commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset('assets/images/Logo.png'),
+          const SizedBox(height: 5),
+          Text('Read free books', textAlign: TextAlign.center),
+        ],
+      ),
+    );
+  }
+}
