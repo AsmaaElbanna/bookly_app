cf5c51438fb8461b0372cc717938bdf06a0302a7
LYVE <asmaa.elbanna@lyveglobal.com>
2024-07-06 18:35:28 +0300

    cashed network image

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,7 +1,7 @@
-import 'package:bookly_app/utils/assets.dart';
+import 'package:bookly_app/utils/widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import '../../../../../utils/app_router.dart';
 
 class CustomBookItem extends StatelessWidget {
@@ -16,17 +16,26 @@ class CustomBookItem extends StatelessWidget {
       },
       child: AspectRatio(
         aspectRatio: 1.5/2.3,
-
-        child: Container(
-          decoration:  BoxDecoration(
+        child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-            image:  DecorationImage(
-              // image: AssetImage(AssetData.testImage),
-              image: NetworkImage(imageURL ?? ''),
-              fit: BoxFit.cover
-            )
+          child: CachedNetworkImage(
+            imageUrl: imageURL!,
+            fit: BoxFit.fill,
+            placeholder: (context, url) => const CustomLoadingIndicator(),
+            errorWidget: (context, url, error) => const Icon(Icons.error_outline),
           ),
-        ),
+        )
+
+        // Container(
+        //   decoration:  BoxDecoration(
+        //   borderRadius: BorderRadius.circular(20),
+        //     image:  DecorationImage(
+        //       // image: AssetImage(AssetData.testImage),
+        //       image: NetworkImage(imageURL ?? ''),
+        //       fit: BoxFit.cover
+        //     )
+        //   ),
+        // ),
       ),
     );
   }
