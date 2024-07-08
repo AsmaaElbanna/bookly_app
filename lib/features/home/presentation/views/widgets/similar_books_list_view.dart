85080ef2e84f29baf596dc3ef692b29a5c006bab
LYVE <asmaa.elbanna@lyveglobal.com>
2024-07-08 10:12:22 +0300

    create similar books

--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,6 +1,10 @@
 import 'package:bookly_app/features/home/presentation/manager/similar_books/similar_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:bookly_app/utils/widgets/custom_error_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../utils/widgets/custom_loading_indicator.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
@@ -8,7 +12,28 @@ class SimilarBooksListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SimilarBooksCubit , SimilarBooksState>(builder: (context, state) {
-      return Text('data');
+      if (state is SimilarBooksSuccess) {
+        return SizedBox(
+          height: MediaQuery.of(context).size.height * .15,
+          child: ListView.builder(
+              itemCount: state.similarBooks.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: CustomBookItem(
+                    imageURL:
+                    state.similarBooks[index].volumeInfo.imageLinks?.thumbnail ??
+                        '',
+                  ),
+                );
+              }),
+        );
+      } else if (state is SimilarBooksFailure) {
+        return CustomErrorWidget(errorMessage: state.errorMessage);
+      } else {
+        return const CustomLoadingIndicator();
+      }
     },);
   }
 }
--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -1,6 +1,11 @@
+import 'package:bookly_app/features/home/data/repo/home_repo.dart';
+import 'package:bookly_app/features/home/data/repo/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books/similar_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/details_screen.dart';
 import 'package:bookly_app/features/home/presentation/views/home_screen.dart';
 import 'package:bookly_app/features/search/presentation/views/search_view.dart';
+import 'package:bookly_app/utils/service_locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/splash/presentation/views/splash_screen.dart';
@@ -14,7 +19,10 @@ abstract class AppRouter {
       routes: [
         GoRoute(path: '/',builder: (context, state) => const SplashScreen(),),
         GoRoute(path: kHomeView,builder: (context, state) => const HomeScreen(),),
-        GoRoute(path: kDetails,builder: (context, state) => const DetailsScreen(),),
+        GoRoute(path: kDetails,builder: (context, state) => BlocProvider(
+          create: (context) => SimilarBooksCubit(homeRepo: getIt.get<HomeRepoImpl>())..fetchSimilarBooks(category: 'category'),
+          child: const DetailsScreen(),
+        ),),
         GoRoute(path: kSearch,builder: (context, state) => const SearchView() )
 
       ]);
